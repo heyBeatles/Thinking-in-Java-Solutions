f93a6f2f1fe086ee8a1d59dd90739985394e664d
heyBeatles <bincanlin@qq.com>
2021-02-02 22:11:54 +0800

    check the ex17 solution with idea

--- Chapter 14 - Type Information/Exercise 17/ch14ex17/ch14ex17/ShowMethods.java
+++ Chapter 14 - Type Information/Exercise 17/ch14ex17/ch14ex17/ShowMethods.java
@@ -4,7 +4,7 @@ package ch14ex17;
 // {Args: ShowMethods}
 import java.lang.reflect.*;
 import java.util.regex.*;
-import static net.mindview.util.Print.*;
+//import static net.mindview.util.Print.*;
 
 public class ShowMethods {
 	private static String usage =
@@ -13,10 +13,11 @@ public class ShowMethods {
 			"To show all methods in class or:\n" +
 			"ShowMethods qualified.class.name word\n" +
 			"To search for methods involving 'word'";
+	//private static Pattern p = Pattern.compile("(\\s*final)|(\\w+\\.)|(\\s*native)");
 	private static Pattern p = Pattern.compile("(\\s*final)|(\\w+\\.)|(\\s*native)");
 	public static void main(String[] args) {
 		if(args.length < 1) {
-			print(usage);
+			System.out.println(usage);
 			System.exit(0);
 		}
 		int lines = 0;
@@ -26,24 +27,24 @@ public class ShowMethods {
 			Constructor[] ctors = c.getConstructors();
 			if(args.length == 1) {
 				for(Method method : methods)
-					print(p.matcher(method.toString()).replaceAll(""));
+					System.out.println(p.matcher(method.toString()).replaceAll(""));
 				for(Constructor ctor : ctors)
-					print(p.matcher(ctor.toString()).replaceAll(""));
+					System.out.println(p.matcher(ctor.toString()).replaceAll(""));
 				lines = methods.length + ctors.length;
 			} else {
 				for(Method method : methods)
 					if(method.toString().indexOf(args[1]) != -1) {
-						print(p.matcher(method.toString()).replaceAll(""));
+						System.out.println(p.matcher(method.toString()).replaceAll(""));
 						lines++;
 					}
 				for(Constructor ctor : ctors)
 					if(ctor.toString().indexOf(args[1]) != -1) {
-						print(p.matcher(ctor.toString()).replaceAll(""));
+						System.out.println(p.matcher(ctor.toString()).replaceAll(""));
 						lines++;
 					}
 			}
 		} catch(ClassNotFoundException e) {
-			print("No such class: " + e);
+			System.out.println("No such class: " + e);
 		}
 	}
 }
